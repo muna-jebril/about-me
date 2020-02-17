b7655572e1ed7a3558611f570bc74f0fc03c974c
muna <muna.jebril1995@hotmail.com>
2020-02-17 20:01:50 +0200

    i added the code of js

---
+++ js/app.js
@@ -0,0 +1,78 @@
+'use strict';
+     
+ var  name=prompt("Is my name Muna?", "yes/y  no/n");
+ 
+ if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+ {
+                  // console.log(name);
+
+     var resultName= alert("you`re right that`s my name");
+ }
+ else if ((name.toUpperCase() =='NO' || name.toUpperCase()=='N') && (name.toLowerCase() =='no' || name.toLowerCase()=='n'))
+ {
+                       // console.log(name);
+
+     var resultName= alert("no you are wrong i`m sorry let`s the anther quation");
+ }
+
+
+ var  food=prompt("Is my fav food mansaf??", "yes/y  no/n");
+ 
+ if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+ {
+                       // console.log(resultfood);
+
+     var resultfood= alert("you`re right,i`m in love with it ");
+ }
+ else 
+ {
+                            // console.log(resultfood);
+
+     var resultfood= alert("no you are wrong you do`nt look that you know me ");
+ }
+
+var  color=prompt("Is my fav color pink??", "yes/y  no/n");
+ 
+ if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+ {
+   // console.log(resultcolor);
+     var resultcolor= alert("you`re wrong,my fav color is red ");
+ }
+ else 
+ {
+     var resultcolor= alert("you`re write my fav color is`nt pink it`s red ");
+        // console.log(resultcolor);
+
+ }
+ 
+ var  age=prompt("Is i`m 24 years old??", "yes/y  no/n");
+ 
+ if((age.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+ {
+        // console.log(resultage);
+
+     var resultage= alert("yes good job");
+ }
+ else 
+ {
+             // console.log(resultage);
+     var resultage= alert("no you`r wrong ");
+ }
+
+  var  gradeation=prompt("Did i graduated in 2019??", "yes/y  no/n");
+ 
+ if((age.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+ {
+             // console.log(esultgradeation);
+
+     var resultgradeation= alert("yes  you`re right");
+ }
+ else 
+ {
+                  // console.log(esultgradeation);
+
+     var resultgradeation= alert("no you`r wrong ");
+ }
+var state= alert("hello user  ");
+var Name= prompt("plz enter your name ");
+var welcome= alert("hello"+ Name+" wlecome to our website ");
