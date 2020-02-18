d64901f2e84b2cd43a982e59bc5529cd4e514eb6
muna <muna.jebril1995@hotmail.com>
2020-02-18 20:33:53 +0200

    finish lab3

--- js/app.js
+++ js/app.js
@@ -1,78 +1,140 @@
-'use strict';
-     
- var  name=prompt("Is my name Muna?", "yes/y  no/n");
- 
- if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
- {
-                  // console.log(name);
+// 'use strict';
+// var result=0;
+//  var  name=prompt("Is my name Muna?", "yes/y  no/n");
 
-     var resultName= alert("you`re right that`s my name");
- }
- else if ((name.toUpperCase() =='NO' || name.toUpperCase()=='N') && (name.toLowerCase() =='no' || name.toLowerCase()=='n'))
- {
-                       // console.log(name);
+//  if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+//  {
+//                   // console.log(name);
+// result++;
+//      var resultName= alert("you`re right that`s my name");
+//  }
+//  else if ((name.toUpperCase() =='NO' || name.toUpperCase()=='N') && (name.toLowerCase() =='no' || name.toLowerCase()=='n'))
+//  {
+//                        // console.log(name);
 
-     var resultName= alert("no you are wrong i`m sorry let`s the anther quation");
- }
+//      var resultName= alert("no you are wrong i`m sorry let`s the anther quation");
+//  }
 
 
- var  food=prompt("Is my fav food mansaf??", "yes/y  no/n");
- 
- if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
- {
-                       // console.log(resultfood);
+//  var  food=prompt("Is my fav food mansaf??", "yes/y  no/n");
 
-     var resultfood= alert("you`re right,i`m in love with it ");
- }
- else 
- {
-                            // console.log(resultfood);
+//  if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+//  {
+//                        // console.log(resultfood);
+// result++;
+//      var resultfood= alert("you`re right,i`m in love with it ");
+//  }
+//  else 
+//  {
+//                             // console.log(resultfood);
 
-     var resultfood= alert("no you are wrong you do`nt look that you know me ");
- }
+//      var resultfood= alert("no you are wrong you do`nt look that you know me ");
+//  }
 
-var  color=prompt("Is my fav color pink??", "yes/y  no/n");
- 
- if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
- {
-   // console.log(resultcolor);
-     var resultcolor= alert("you`re wrong,my fav color is red ");
- }
- else 
- {
-     var resultcolor= alert("you`re write my fav color is`nt pink it`s red ");
-        // console.log(resultcolor);
+// var  color=prompt("Is my fav color pink??", "yes/y  no/n");
 
- }
- 
- var  age=prompt("Is i`m 24 years old??", "yes/y  no/n");
- 
- if((age.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
- {
-        // console.log(resultage);
+//  if((name.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+//  {
+//    // console.log(resultcolor);
+//      var resultcolor= alert("you`re wrong,my fav color is red ");
+//      result++;
 
-     var resultage= alert("yes good job");
- }
- else 
- {
-             // console.log(resultage);
-     var resultage= alert("no you`r wrong ");
- }
+//  }
+//  else 
+//  {
+//      var resultcolor= alert("you`re right my fav color is`nt pink it`s red ");
+//         // console.log(resultcolor);
 
-  var  gradeation=prompt("Did i graduated in 2019??", "yes/y  no/n");
- 
- if((age.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
- {
-             // console.log(esultgradeation);
+//  }
 
-     var resultgradeation= alert("yes  you`re right");
- }
- else 
- {
-                  // console.log(esultgradeation);
+//  var  age=prompt("Is i`m 24 years old??", "yes/y  no/n");
 
-     var resultgradeation= alert("no you`r wrong ");
- }
-var state= alert("hello user  ");
-var Name= prompt("plz enter your name ");
-var welcome= alert("hello"+ Name+" wlecome to our website ");
+//  if((age.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+//  {
+//         // console.log(resultage);
+//         result++;
+
+
+//      var resultage= alert("yes good job");
+//  }
+//  else 
+//  {
+//              // console.log(resultage);
+//      var resultage= alert("no you`r wrong ");
+//  }
+
+//   var  gradeation=prompt("Did i graduated in 2019??", "yes/y  no/n");
+
+//  if((age.toUpperCase() =='YES' || name.toUpperCase()=='Y') && (name.toLowerCase() =='yes' || name.toLowerCase()=='y'))
+//  {
+//              // console.log(esultgradeation);
+//              result++;
+
+//      var resultgradeation= alert("yes  you`re right");
+//  }
+//  else 
+//  {
+//                   // console.log(esultgradeation);
+
+//      var resultgradeation= alert("no you`r wrong ");
+//  }
+//  var number= prompt("guess what  is the number ?")
+//  var atempts=0;
+//  var msg;
+// while (atempts<4)
+// {
+//     if(number<4 ){
+//         alert("too low ");
+//         console.log("the num is ",number);
+//         var number= prompt("try agine ?");
+//         atempts=atempts+1;
+//         console.log( "attep",atempts);}
+
+
+
+//      else if (number>4){
+
+
+//         alert("too high");
+//         console.log("the num is ",number);
+//         var number= prompt("try again ?");
+//         atempts=atempts+1;
+//         console.log( "att",atempts);
+
+//     }
+//     else if (number==4)
+//     {
+//         var answer =alert("good job the number is 4 ");
+//         result++;
+
+//         break;
+//     }
+
+
+// }   
+// //alert("game over the number is 4");
+
+// if(atempts==4){
+//  msg= alert("game over the number is 4");
+// }
+// var answer;
+// var favcars = ["BMW", "maracdes", "bejo", "kia", "hounda",];
+// answer=prompt("enter the name of my car ?");
+// for(var counter=0;counter <6; counter++)
+// {
+// if(favcars[0]==answer ||favcars[1]==answer || favcars[2]==answer || favcars[3]==answer ||favcars[4]==answer){
+//     result++;
+
+// alert("my av cars are BMW, maracdes, bejo, kia, hounda");
+// break;
+// }
+// else 
+// prompt("try agine");
+
+// }
+// if(counter==6)
+// alert("sorry you have tried many times");
+// var state= alert("hello ");
+// var Name= prompt("plz enter your name ");
+// var welcome= alert("hello"+ Name+" wlecome to our website ");
+// alert(`your final resault is  ${result}`);
